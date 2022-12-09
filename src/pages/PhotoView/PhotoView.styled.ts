abf7c41d8317cf2ff883bb6562d584f70c191dcc
nahhuaraujo <nahhuaraujo@gmail.com>
2022-12-08 23:58:08 -0300

    Add new PhotoView page

---
+++ src/pages/PhotoView/PhotoView.styled.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { defaultButtonStyle, mainColorMid, defaultPageStyle } from '../../styles/global-styled-css';
+
+export const PhotoView = styled.main`
+  ${defaultPageStyle}
+`;
+
+export const PhotoViewContainer = styled.section`
+  width: 90%;
+  padding: 1rem;
+  margin: 0 auto;
+  border-radius: 1rem;
+  background-color: ${mainColorMid};
+  transition: 300ms;
+
+  :hover {
+    box-shadow: inset 0 -3rem 3rem rgba(0, 0, 0, 0.1), 0.3rem 0.3rem 1rem rgba(0, 0, 0, 0.3);
+  }
+`;
+
+export const PhotoViewImg = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
+export const PhotoViewActions = styled.div`
+  margin-top: 1rem;
+  bottom: 0;
+  display: flex;
+  justify-content: end;
+
+  button {
+    margin-left: 1rem;
+  }
+`;
+
+export const PhotoViewDescription = styled.section`
+  margin-top: 1rem;
+`;
+
+export const PhotoBackButton = styled.button`
+  ${defaultButtonStyle};
+`;
+
+export const PhotoShareButton = styled.button`
+  ${defaultButtonStyle};
+`;
---
+++ src/pages/PhotoView/PhotoView.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { NavLink } from '../../components';
+import { gallery } from '../../data';
+import { PublicRoutes } from '../../routes';
+import * as S from './PhotoView.styled';
+
+interface Photo {
+  id: number;
+  description: string;
+  img: string;
+}
+
+const PhotoView = () => {
+  const [photo, setPhoto] = useState<Photo>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const selectedPhoto = gallery.find(photo => photo.id === Number.parseInt(id as string));
+    if (selectedPhoto) return setPhoto(selectedPhoto as any);
+  }, [id]);
+
+  return (
+    <S.PhotoView>
+      <S.PhotoViewContainer>
+        <S.PhotoViewImg>
+          <img src={photo?.img} alt={photo?.description} title={photo?.description} />
+        </S.PhotoViewImg>
+        <S.PhotoViewDescription>{photo?.description}</S.PhotoViewDescription>
+        <S.PhotoViewActions>
+          <NavLink to={`/${PublicRoutes.GALLERY}`} type='button'>
+            Back
+          </NavLink>
+          <S.PhotoShareButton>Share</S.PhotoShareButton>
+        </S.PhotoViewActions>
+      </S.PhotoViewContainer>
+    </S.PhotoView>
+  );
+};
+
+export default PhotoView;
